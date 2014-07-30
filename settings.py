93f76758cd625171dd93ed4b10c53e7ccb41f3ab
JmPotato <ghzpotato@gmail.com>
2014-07-30 15:40:53 +0800

    Add settings

---
+++ settings.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+google_analytics = ''
+cookie_secret = 'RESET ME!!!!'
+
+#MongoDB Settings
+mongodb_host = '127.0.0.1'
+mongodb_port = 27017
+database_name = 'bookshelf'
+
+#Development Settings
+Debug = True
